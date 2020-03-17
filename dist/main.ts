ca27591a078040da00c98fb74ae1813677178027
Lapis Lapis <Iel Villaruel>
2020-03-17 21:45:45 +0800

    first commit

--- dist/main.ts
+++ dist/main.ts
@@ -1,4 +1,4 @@
-//I don't have Integ.Prog thats why i don't have any functionalities in my form
+//I don't have Integ.Prog thats why i don't have any functionalities in my form other than the display name and lastname.
 document.getElementById('send').addEventListener('click', updateValue);
 function updateValue() {
     var name = document.getElementById('name').value;
